43a0caad2fb0abc016f537100bb92aeb31c60636
SidOTU <sidharthsuresh07@gmail.com>
2024-04-28 03:02:38 -0400

    added \n and \t

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,2 +1,27 @@
 extends Control
 
+@export var TextEditObject: TextEdit = null
+
+
+func _on_literal_button_button_down():
+	DisplayServer.clipboard_set(LiteralExtractor(TextEditObject.text))
+
+
+func _on_clear_button_button_up():
+	TextEditObject.set_text("")
+
+
+func _on_copy_button_button_down():
+	DisplayServer.clipboard_set(TextEditObject.get_text())
+
+
+func LiteralExtractor(text: String) -> String:
+	var converted = ""
+	for i in range(len(text)):
+		if text[i] == "\n":
+			converted += "\\n"
+		elif text == "\t":
+			converted += "\\t"
+		else:
+			converted += text[i]
+	return converted
